488ac7ce7a4cdd0b6335a25be5a6937e78d543f1
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2023-06-10 12:37:33 +0200

    feat(manifest): ox and oal

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,10 +1,11 @@
 fx_version 'cerulean'
 game 'gta5'
 
-description 'QB-Scrapyard'
+description 'QBX-Scrapyard'
 version '1.0.0'
 
 shared_script {
+    '@ox_lib/init.lua',
     '@PolyZone/client.lua',
     '@PolyZone/BoxZone.lua',
     '@PolyZone/ComboZone.lua',
@@ -12,10 +13,14 @@ shared_script {
     '@qb-core/shared/locale.lua',
     'locales/en.lua', -- Change to the language you want
 }
+
 server_scripts {
     '@oxmysql/lib/MySQL.lua',
     'server/main.lua'
 }
+
 client_script 'client/main.lua'
 
+provide 'qb-scrapyard'
 lua54 'yes'
+use_experimental_fxv2_oal 'yes'
