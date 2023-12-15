a3da00eac9c9b58118122fc3a85f637d3b815070
shanruto <dalavanja025@gmail.com>
2023-12-15 16:38:25 +0100

    refactor: update to ox, remove thread, add blips

--- locales/en.lua
+++ locales/en.lua
@@ -1,28 +1,26 @@
 local Translations = {
     error = {
-        smash_own = "You can't smash a vehicle that owns it.",
-        cannot_scrap = "This Vehicle Cannot Be Scrapped.",
-        not_driver = "You Are Not The Driver",
-        demolish_vehicle = "You Are Not Allowed To Demolish Vehicles Now",
-        canceled = "Canceled",
+        scrap_owned = 'You can\'t scrap player owned vehicles.',
+        cannot_scrap = 'This vehicle cannot be scrapped.',
+        not_driver = 'You are not the driver',
+        scrap_vehicle = 'You are not allowed to scrap vehicles now',
+        canceled = 'Canceled',
     },
     text = {
-        scrapyard = 'Scrap Yard',
         disassemble_vehicle = '[E] - Disassemble Vehicle',
-        disassemble_vehicle_target = 'Disassemble Vehicle',
-        email_list = "[E] - E-mail Vehicle List",
-        email_list_target = "E-mail Vehicle List",
-        demolish_vehicle = "Demolish Vehicle",
-        email_sent = "You will receive the list by email in a few seconds",
+        email_list = '[E] - E-mail Vehicle List',
+        email_list_target = 'E-mail Vehicle List',
+        scrap_vehicle = 'Scrap Vehicle',
+        email_sent = 'You will receive the list by email in a few seconds',
     },
     email = {
-        sender = "Turner’s Auto Wrecking",
-        subject = "Vehicle List",
-        message = "You Can Only Demolish A Number Of Vehicles.<br />You Can Keep Everything You Demolish For Yourself As Long As You Dont Bother Me.<br /><br /><strong>Vehicle List:</strong><br />",
+        sender = 'Turner\'s Auto Wrecking',
+        subject = 'Vehicle List',
+        message = 'You can only scrap a limited number of vehicles..<br />You can keep everything you scrap for yourself as long as you don\'t bother me.<br /><br /><strong>Vehicle List:</strong><br />',
     },
 }
 
-Lang = Locale:new({
+Lang = Lang or Locale:new({
     phrases = Translations,
     warnOnMissing = true
 })
